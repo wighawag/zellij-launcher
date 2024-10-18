fbfc49e99a33f2dd0d61b6fcd58f62cab5e2b51f
wighawag <wighawag@gmail.com>
2024-10-18 12:32:28 +0100

    no deps

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,6 @@
 
 import { exec, execFileSync } from "child_process";
 import { promisify } from "util";
-import { Command } from "commander";
 
 const execAsync = promisify(exec);
 
@@ -17,52 +16,54 @@ const require = createRequire(import.meta.url);
 const pkg = require("../package.json");
 
 async function main() {
-  const program = new Command();
+  const args = process.argv.slice(2);
 
-  program
-    .version(pkg.version)
-    .name(pkg.name)
-    .description(pkg.description)
-    .argument("<session>", "specify session to use")
-    .parse(process.argv);
-
-  const options = program.opts();
-  const args = program.args;
-  const sessionToLaunch = args[0];
-
-  try {
-    const { stdout, stderr } = await execAsync("zellij list-sessions");
-    const lines = stdout.trim().split("\n");
-    for (const lineWithAnsi of lines) {
-      const line = stripAnsi(lineWithAnsi);
-      const firstSpaceIndex = line.indexOf(" ");
-      const session = line.substring(0, firstSpaceIndex);
-      if (session == sessionToLaunch) {
-        console.log(session);
-        if (line.indexOf("(EXITED") >= 0) {
-          console.log(`deleting session named "${session}"...`);
-          await execAsync(`zellij delete-session ${session}`);
-        }
+  let sessionToLaunch: string | undefined;
+  let attaching = false;
+  for (const arg of args) {
+    if (attaching) {
+      sessionToLaunch = arg;
+      attaching = false;
+    } else {
+      if (arg == "attach" || arg == "a") {
+        attaching = true;
       }
     }
-  } catch (err: any) {
-    if (
-      err.stderr &&
-      stripAnsi(err.stderr.trim()) == "No active zellij sessions found."
-    ) {
-      //   console.log(JSON.stringify(err));
-    } else {
-      throw err;
+  }
+
+  if (sessionToLaunch) {
+    try {
+      const { stdout, stderr } = await execAsync("zellij list-sessions");
+      const lines = stdout.trim().split("\n");
+      for (const lineWithAnsi of lines) {
+        const line = stripAnsi(lineWithAnsi);
+        const firstSpaceIndex = line.indexOf(" ");
+        const session = line.substring(0, firstSpaceIndex);
+        if (session == sessionToLaunch) {
+          console.log(session);
+          if (line.indexOf("(EXITED") >= 0) {
+            console.log(`deleting session named "${session}"...`);
+            await execAsync(`zellij delete-session ${session}`);
+          }
+        }
+      }
+    } catch (err: any) {
+      if (
+        err.stderr &&
+        stripAnsi(err.stderr.trim()) == "No active zellij sessions found."
+      ) {
+      } else {
+        throw err;
+      }
     }
   }
 
   try {
-    const result = execFileSync("zellij", [], {
+    execFileSync("zellij", args, {
       stdio: "inherit",
     });
-    console.log(result);
-  } catch (err) {
-    console.error(`failed to execute 'zellij'`);
+  } catch (err: any) {
+    process.exit(err.status || 1);
   }
 }
 
